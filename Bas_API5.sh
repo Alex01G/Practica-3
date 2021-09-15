1fb32c34da26be067b8d794ae78948ca7b067853
Alex01G <89890973+Alex01G@users.noreply.github.com>
2021-09-14 23:23:05 -0500

    Create Bas_API5.sh

---
+++ Bas_API5.sh
@@ -0,0 +1,5 @@
+curl -i https://api.github.com/users/ppGodel/repos
+#Misma metodología para sacar links de un perfil especificado de github
+
+https://api.github.com/users/ppGodel/followers
+# El url que se escogió esta vez fue el de Followers, el cual, nos proporciona el usuario, el id, el node_id y más cosas de cada seguidor de la cuenta seleccionada
