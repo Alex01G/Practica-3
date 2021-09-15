7d904c3f17673e86954094136038d3f72fe5f6c1
Alex01G <89890973+Alex01G@users.noreply.github.com>
2021-09-14 23:10:48 -0500

    Update API_1.py

--- API_1.py
+++ API_1.py
@@ -20,3 +20,8 @@ for key in hunter["data"]:
                 print (sourc,"\t",URL,"\tstatus:",pagestat.status_code) 
             else: 
                 print (sourc,"\t",URL,"\tstatus:",pagestat.status_code, "Falló")
+                
+"""Al ingresar un apikey, el scrip tomará el valor de la variable del correo para que por medio de hunter.io poder acceder a valiosa información.
+La primera parte se encarga de verificar el código de error, si da 200 puede proseguir con normalidad.
+Luego arroja variables cómo: status para verificar si es un correo válido, la capacidad de entrega cómo score, si el correo pasa sus expresiones regulares, etc.
+Luego te da un listado de las fuentes dónde se encontró el código más su correspondiente código de error."""
